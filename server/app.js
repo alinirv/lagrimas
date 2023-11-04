8307cd3801732398ea106191941c7213db70231f
alinirv <alini.vicente@yahoo.com.br>
2023-11-04 11:56:16 -0300

    feat: create routes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/../public'));
+const router = require('../src/routes/routes.js')
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/',router);
+
+
 
 app.get('/', (req, res) => {
     res.send('Servidor Express funcionando!');
