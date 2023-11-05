cf208c0c92362054c77bb4f3fe794763702e59d3
alinirv <alini.vicente@yahoo.com.br>
2023-11-05 13:43:51 -0300

    fix: adjusts MIME type for JavaScript files

--- server/app.js
+++ server/app.js
@@ -4,17 +4,11 @@ const port = 3000;
 
 const router = require('../src/routes/routes')
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use('/',router);
 
 
-// Middleware para definir o tipo MIME correto para arquivos JavaScript
-app.use('../public/script', (req, res, next) => {
-  res.setHeader('Content-Type', 'text/javascript');
-  next();
-});
-
-  
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
