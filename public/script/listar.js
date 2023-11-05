02033865416dfe5e8c7b9bc6a049353e1b545e66
alinirv <alini.vicente@yahoo.com.br>
2023-11-05 16:09:44 -0300

    refactor: adjusting the list function

--- public/script/listar.js
+++ public/script/listar.js
@@ -1,29 +1,28 @@
-
 // Função para criar um elemento de card para uma lágrima
 function criarCardLagrima(lagrima) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    
-    card.innerHTML = `
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
         <h3>${lagrima.nome}</h3>
         <p>Categoria: ${lagrima.categoria}</p>
         <p>Autor: ${lagrima.autor}</p>
     `;
-    
-    return card;
+
+  return card;
 }
 
-// Função para exibir as lágrimas na página
-function exibirLagrimas() {
-    let listLagrimas = localStorage.getItem('lagrimas');
-    const lagrimas = JSON.parse(listLagrimas);
-    const lagrimasContainer = document.getElementById('lagrimas-container');
-    
-    lagrimas.forEach((lagrima) => {
+function getLagrimas() {
+  fetch("/api/lagrimas")
+    .then((response) => response.json())
+    .then((lagrimas) => {
+      const lagrimasContainer = document.getElementById("lagrimas-container");
+      lagrimas.forEach((lagrima) => {
         const card = criarCardLagrima(lagrima);
         lagrimasContainer.appendChild(card);
+      });
     });
 }
 
-// Chamada da função para exibir as lágrimas quando a página carregar
-window.addEventListener('load', exibirLagrimas);
+// Chamada da função para buscar as lágrimas quando a página carregar
+window.addEventListener("load", getLagrimas);
