523ed19f82605fcf750641c4cc0a0dbd8d2485c5
alinirv <alini.vicente@yahoo.com.br>
2023-11-05 10:58:03 -0300

    refactor: route adjustments

--- server/app.js
+++ server/app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const router = require('../src/routes/routes.js')
+const router = require('../src/routes/routes')
 
-app.use(express.static('public'));
 app.use(express.json());
 app.use('/',router);
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,4 +2,14 @@ const express = require('express');
 const router =express.Router();
 
 
+router.get('/', (req, res) =>{
+    res.sendFile('index.html', {root:'public'})
+})
+
+router.get('/listar', (req, res) =>{
+    res.sendFile('listar.html', {root:'src/views'})
+})
+
+
+
 module.exports =router;
